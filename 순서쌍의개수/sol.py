ff5bc27872ea9377116f7068fbd737b582b3bdaa
seongjin <seongjin5743@naver.com>
2025-02-18 17:00:01 +0900

    qwer

--- 순서쌍의개수/sol.py
+++ 순서쌍의개수/sol.py
@@ -1,6 +1,16 @@
 def solution(n):
-    answer=0
-    for i in range(1,n+1):
-        if n%i==0:
-            answer+=1
+    answer = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            answer += 1
+    return answer
+
+import math
+def solution1(n):
+    answer = 0
+    for i in range(1,  int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            answer += 2
+            if i * i == n:
+                answer -= 1
     return answer
--- 최댓값만들기1/sol.py
+++ 최댓값만들기1/sol.py
@@ -1,3 +1,15 @@
 def solution(numbers):
     numbers.sort()
     return numbers[-1] * numbers[-2]
+
+def solution1(numbers):
+    numbers.sort(reverse = True)
+    return numbers[0] * numbers[1]
+
+def solution2(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] > numbers[i + 1]:
+                numbers[j], numbers[i + 1] = numbers[i + 1], numbers[j]
+    return numbers[-1] * numbers[-2]
