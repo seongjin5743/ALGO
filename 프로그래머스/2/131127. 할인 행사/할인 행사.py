a8cffac92d76ba293615f39cc2646f8c6d28af21
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-07-29 07:35:58 +0900

    [level 2] Title: 할인 행사, Time: 271.44 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,29 +1,39 @@
+from collections import Counter
+
 def solution(want, number, discount):
     answer = 0
     fruits = []
     
+#     for w, n in zip(want, number):
+#         fruits.extend([w] * n)
+    
+#     fruits.sort()
+    
+#     for i in range(len(discount) - 9):
+#         qwer = discount[i:i+10]
+#         qwer.sort()
+#         if fruits == qwer:
+#             answer += 1
+        
+        
+    # for i in range(len(discount) - 9):
+    #     flag = False
+    #     qwer = discount[i:i+10]
+    #     for j in range(len(want)):
+    #         if qwer.count(want[j]) >= number[j]:
+    #             flag = True
+    #         else:
+    #             flag = False
+    #             break
+    #     if flag:
+    #         answer += 1
+    #     fruits = []
+    
     for w, n in zip(want, number):
         fruits.extend([w] * n)
-    
-    fruits.sort()
-    
+        
     for i in range(len(discount) - 9):
         qwer = discount[i:i+10]
-        qwer.sort()
-        if fruits == qwer:
+        if Counter(fruits) == Counter(qwer):
             answer += 1
-        
-        
-#     for i in range(len(discount) - 10):
-#         flag = False
-#         qwer = discount[i:i+10]
-#         for j in range(len(want)):
-#             if qwer.count(want[j]) >= number[j]:
-#                 flag = True
-#             else:
-#                 flag = False
-
-#         if flag:
-#             answer += 1
-            
     return answer
