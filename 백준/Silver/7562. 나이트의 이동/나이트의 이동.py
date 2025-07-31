4cbb73587d6c9a9cbf11bd531f8d562d35bb373b
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-07-31 13:45:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 1084 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -4,19 +4,20 @@ n = int(input())
 
 for _ in range(n):
     l = int(input())
+
     x, y = map(int, input().split())
     fx, fy = map(int, input().split())
-    
-    visited = [[False] * l for _ in range(l)]
 
     dx = [-2, -1, 1, 2, 2, 1, -1, -2]
     dy = [1, 2, 2, 1, -1, -2, -2, -1]
 
-    def bfs(x, y):
+    visited = [[False] * l for _ in range(l)]
+    
+    def bfs(x, y, visited, count):
         queue = deque()
-        queue.append((x, y, 0))
+        queue.append((x,y,count))
         visited[x][y] = True
-
+        
         while queue:
             x, y, count = queue.popleft()
 
@@ -26,10 +27,12 @@ for _ in range(n):
             for i in range(8):
                 nx, ny = x + dx[i], y + dy[i]
 
-                if nx < 0 or ny < 0 or ny >= l or nx >= l:
+                if nx < 0 or ny < 0 or nx >= l or ny >= l:
                     continue
 
                 if not visited[nx][ny]:
-                    queue.append((nx, ny,count + 1))
                     visited[nx][ny] = True
-    print(bfs(x, y))
+                    queue.append((nx, ny, count + 1))
+
+    print(bfs(x, y, visited, 0))
+    
