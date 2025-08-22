6c6d805b06f535ea3f9ee646a956c3838a4734f8
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-08-22 11:26:57 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 2476 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -1,11 +1,7 @@
 n, game = input().split()
 n = int(n)
 
-players = set()
-
-for _ in range(n):
-    players.add(input())
-
+players = set(input() for _ in range(n))
 games = {'Y': 1, 'F': 2, 'O': 3}
 
 print(len(players) // games[game])
