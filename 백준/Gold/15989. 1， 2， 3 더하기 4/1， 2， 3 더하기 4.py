805cb47407bbdd4e445b4197018e0aa0fd6b6710
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-08-22 17:13:58 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,14 +1,12 @@
 t = int(input())
 
-dp = [[0] * 10001 for _ in range(4)]
-dp[0][0] = 1
+dp = [0] * 10001
+dp[0] = 1
 
 for i in range(1, 4):
-    for j in range(10001):
-        dp[i][j] = dp[i - 1][j]
+    for j in range(i, 10001):
+        dp[j] += dp[j - i]
 
-        if j >= i:
-            dp[i][j] += dp[i][j - i]
 for _ in range(t):
     num = int(input())
-    print(dp[3][num])
+    print(dp[num])
