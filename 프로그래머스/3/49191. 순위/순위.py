487e804df284d66c245de1fad73f9cb39cc161f2
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-09-22 08:08:42 +0900

    [level 3] Title: 순위, Time: 194.13 ms, Memory: 9.74 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,21 @@
+def solution(n, results):
+    answer = 0
+    
+    distance = [[0] * n for _ in range(n)]
+    
+    for x, y in results:
+        distance[x - 1][y - 1] = 1
+        distance[y - 1][x - 1] = -1
+        
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if distance[i][k] == distance[k][j] == 1:
+                    distance[i][j] = 1
+                elif distance[i][k] == distance[k][j] == -1:
+                    distance[i][j] = -1
+    for result in distance:
+        if result.count(0) == 1:
+            answer += 1
+    print(distance)
+    return answer
