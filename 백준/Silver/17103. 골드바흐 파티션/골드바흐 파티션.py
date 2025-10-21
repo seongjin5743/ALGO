286109ac4306b1e54dec6f0829322b8a2dedf3d9
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-10-21 17:49:18 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 2680 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+m = 1000000
+prime = [True] * (m + 1)
+prime[0], prime[1] = False, False
+
+for i in range(2, int(m ** 0.5) + 1):
+    if prime[i]:
+        for j in range(i * i, m + 1, i):
+            prime[j] = False
+
+for _ in range(t):
+    n = int(input())
+    count = 0
+    for i in range(n // 2, 1, -1):
+        if prime[i] and prime[n - i]:
+            count += 1
+
+    print(count)
