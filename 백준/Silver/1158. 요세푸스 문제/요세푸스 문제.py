67073b3f228d7f5b436227f3891f67c7072841ff
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2025-11-11 13:46:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+index = 0
+array = list(range(1, N + 1))
+result = []
+
+while len(array) != 0:
+    index += (K - 1)
+    index = index % len(array)
+    result.append(array.pop(index))
+
+print('<', end='')
+for i in range(N - 1):
+    print(result[i], end=', ')
+print(result[-1], end = '')
+print('>',end='')
