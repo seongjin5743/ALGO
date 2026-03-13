51a6c93a95e4c4ab8ac9e99a27062c17b5dee5fc
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-03-13 15:45:06 +0900

    [Gold V] Title: 상범 빌딩, Time: 112 ms, Memory: 35616 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -1,50 +1,51 @@
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-dz = [0, 0, 0, 0, 1, -1]
-dx = [1, -1, 0, 0, 0, 0]
-dy = [0, 0, 1, -1, 0, 0]
+dx = [1,-1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,1,-1]
 
 while True:
-    l, r, c = map(int, input().split())
+    l,r,c = map(int, input().split())
+
     if l == 0 and r == 0 and c == 0:
         break
 
     maze = []
+
     for _ in range(l):
         floor = [list(input().strip()) for _ in range(r)]
         maze.append(floor)
-        _ = input().strip()  # 층 사이의 빈 줄 처리
-
+        _ = input().strip()
+    
     visited = [[[False] * c for _ in range(r)] for _ in range(l)]
 
     def bfs(x, y, z):
-        queue = deque([(x, y, z, 0)])
+        queue = deque()
+        queue.append((x, y, z, 0))
         visited[z][y][x] = True
 
         while queue:
-            x, y, z, cnt = queue.popleft()
+            x, y, z, count = queue.popleft()
+            
+            if maze[z][y][x] == 'E':
+                print(f'Escaped in {count} minute(s).')
+                return
+
             for i in range(6):
                 nx, ny, nz = x + dx[i], y + dy[i], z + dz[i]
-                if 0 <= nx < c and 0 <= ny < r and 0 <= nz < l and not visited[nz][ny][nx]:
-                    if maze[nz][ny][nx] == '.':
-                        visited[nz][ny][nx] = True
-                        queue.append((nx, ny, nz, cnt + 1))
-                    elif maze[nz][ny][nx] == 'E':
-                        return f'Escaped in {cnt + 1} minute(s).'
-        return 'Trapped!'
 
-    found = False
+                if 0 <= nx < c and 0 <= ny < r and 0 <= nz < l:
+                    if not visited[nz][ny][nx] and maze[nz][ny][nx] != '#':
+                        queue.append((nx, ny, nz, count + 1))
+                        visited[nz][ny][nx] = True
+        print('Trapped!')
+
     for z in range(l):
         for y in range(r):
             for x in range(c):
                 if maze[z][y][x] == 'S':
-                    print(bfs(x, y, z))
-                    found = True
+                    bfs(x,y,z)
                     break
-            if found:
-                break
-        if found:
-            break
