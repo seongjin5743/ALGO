60f5cf99ff60ef320d9119c786c8862549d29b24
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-03-13 15:14:48 +0900

    [Gold V] Title: 토마토, Time: 2152 ms, Memory: 100520 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,34 +1,38 @@
+import sys
 from collections import deque
 
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+input = sys.stdin.readline
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+m, n = map(int, input().split())
+
+maze = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 queue = deque()
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            queue.append((i, j))
+
+for y in range(n):
+    for x in range(m):
+        if maze[y][x] == 1:
+            queue.append((x, y))
 
 while queue:
     x, y = queue.popleft()
 
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx, ny = x + dx[i], y + dy[i]
 
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-            graph[nx][ny] = graph[x][y] + 1
+        if 0 <= nx < m and 0 <= ny < n and maze[ny][nx] == 0:
             queue.append((nx, ny))
+            maze[ny][nx] = maze[y][x] + 1
 
-result = 0
-for row in graph:
-    for value in row:
-        if value == 0:
-            print(-1)
-            exit()
-    result = max(result, max(row))
+answer = 0
 
-print(result - 1)
+for row in maze:
+    if 0 in row:
+        print(-1)
+        break
+    answer = max(answer, max(row))
+else:
+    print(answer - 1)
