1bd4f43ebb97f9ea52612c35ae43332b65bf1807
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-04-02 15:44:48 +0900

    [Bronze IV] Title: 모비스, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+alpha = list(input())
+
+if 'M' in alpha and 'O' in alpha and 'B' in alpha and 'I' in alpha and 'S' in alpha:
+    print('YES')
+else:
+    print('NO')
