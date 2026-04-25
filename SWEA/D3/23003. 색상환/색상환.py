cf1539056a99363f18d917c89630418a4c8a10b0
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-04-25 12:46:19 +0900

    [D3] Title: 색상환, Time: 61 ms, Memory: 50,048 KB -BaekjoonHub

--- SWEA/D3/23003. 색상환/색상환.py
+++ SWEA/D3/23003. 색상환/색상환.py
@@ -2,19 +2,20 @@ t = int(input())
 
 color = [input().split() for _ in range(t)]
 
-lst = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+color_map = {
+    'red': 0,
+    'orange': 1,
+    'yellow': 2,
+    'green': 3,
+    'blue': 4,
+    'purple': 5
+}
 
 for c1, c2 in color:
     if c1 == c2:
         print('E')
     else:
-        num1, num2 = 0, 0
-        for i in range(6):
-            if lst[i] == c1:
-                num1 = i
-            if lst[i] == c2:
-                num2 = i
-        num = abs(num1 - num2)
+        num = abs(color_map[c1] - color_map[c2])
 
         if num == 1 or num == 5:
             print('A')
