a9418b89cd74228d482daf9653633a22ecfd51ab
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-04-26 12:05:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 163 ms, Memory: 66,048 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
@@ -1,11 +1,8 @@
-
 from collections import deque
 
+T = int(input())
 
-
-t = int(input())
-
-for i in range(1, t + 1):
+for t in range(1, T + 1):
     n, k = map(int, input().split())
     maze = [list(map(int, input().split())) for _ in range(n)]
 
@@ -22,37 +19,27 @@ for i in range(1, t + 1):
 
     def dfs(x, y, length, used):
         global answer
-        
         answer = max(answer, length)
-        
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
             
             if 0 <= nx < n and 0 <= ny < n and not visited[ny][nx]:
-                
-                # 1. 그냥 이동
-                if maze[y][x] > maze[ny][nx]:
+                if maze[ny][nx] < maze[y][x]:
                     visited[ny][nx] = True
                     dfs(nx, ny, length + 1, used)
                     visited[ny][nx] = False
-                
-                # 2. 깎아서 이동
                 elif not used and maze[ny][nx] - k < maze[y][x]:
                     original = maze[ny][nx]
                     maze[ny][nx] = maze[y][x] - 1
-                    
                     visited[ny][nx] = True
                     dfs(nx, ny, length + 1, True)
                     visited[ny][nx] = False
-                    
                     maze[ny][nx] = original
-
     answer = 0
-
     for x, y in result:
         visited = [[False] * n for _ in range(n)]
         visited[y][x] = True
         dfs(x, y, 1, False)
 
-    print(f'#{i} {answer}')
+    print(f'#{t} {answer}')
