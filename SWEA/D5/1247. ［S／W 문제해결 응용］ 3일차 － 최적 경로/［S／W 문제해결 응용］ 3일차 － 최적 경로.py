57f8a2e50eb48531d26349aa201698d787043e01
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-04-30 08:48:54 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 966 ms, Memory: 77,184 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
@@ -1,48 +1,41 @@
-def get_dist(point1, point2):
-    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-
-
-def dfs(r, c, dist, count):
-    global res
-
-    if dist >= res:
-        return
-
-    if count == N:
-        res = min(res, dist + get_dist((r, c), home))
-        return
-
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            point = points[i]
-
-            dfs(point[0], point[1],
-                dist + get_dist((r, c), point),
-                count + 1)
-
-            visited[i] = False
-
-
 T = int(input())
 
-for test_case in range(1, T + 1):
-    res = float('inf')
+for t in range(1, T + 1):
+    n = int(input())
+    place = list(map(int, input().split()))
 
-    N = int(input())
-    temp = list(map(int, input().split()))
+    s_x, s_y = place[:2]
+    e_x, e_y = place[2:4]
 
-    company = (temp[0], temp[1])
-    home = (temp[2], temp[3])
+    xy = []
+    for i in range(4, len(place), 2):
+        x, y = place[i:i+2]
+        xy.append((x, y))
 
-    points_list = temp[4:]
-    points = []
+    visited = [False] * n
+    answer = float('inf')
 
-    for i in range(0, len(points_list), 2):
-        points.append((points_list[i], points_list[i + 1]))
+    def dfs(x, y, dist, cnt):
+        global answer
 
-    visited = [False] * N
+        if dist >= answer:
+            return
 
-    dfs(company[0], company[1], 0, 0)
+        if cnt == n:
+            dist += abs(x - e_x) + abs(y - e_y)
+            answer = min(answer, dist)
+            return
 
-    print(f"#{test_case} {res}")
+        for i in range(n):
+            if not visited[i]:
+                nx, ny = xy[i]
+                visited[i] = True
+
+                d = abs(x - nx) + abs(y - ny)
+                dfs(nx, ny, dist + d, cnt + 1)
+
+                visited[i] = False
+
+    dfs(s_x, s_y, 0, 0)
+
+    print(f"#{t} {answer}")
