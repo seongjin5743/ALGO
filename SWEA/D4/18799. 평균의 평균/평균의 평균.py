5b38bf306e9f3045974a2b98775c1dfe19f9ecba
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-05-05 17:05:47 +0900

    [D4] Title: 평균의 평균, Time: 68 ms, Memory: 50,816 KB -BaekjoonHub

--- SWEA/D4/18799. 평균의 평균/평균의 평균.py
+++ SWEA/D4/18799. 평균의 평균/평균의 평균.py
@@ -3,9 +3,10 @@ T = int(input())
 for t in range(1, T + 1):
     n = int(input())
     S = list(map(int, input().split()))
-    answer = sum(S) / n
     
-    if answer == sum(S) // n:
-        print(f"#{t} {sum(S) // n}")
+    total = sum(S)
+    
+    if total % n == 0:
+        print(f"#{t} {total // n}")
     else:
-        print(f"#{t} {answer}")
+        print(f"#{t} {total / n}")
