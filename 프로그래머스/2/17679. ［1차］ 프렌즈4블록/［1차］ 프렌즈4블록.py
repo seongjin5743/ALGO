872df7b2f6d9001cebd7c32716cad74bf23e334b
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-06-15 16:05:01 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 59.75 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,31 +1,35 @@
 def solution(m, n, board):
     answer1 = 0
     board = [list(row) for row in board]
-    
     while True:
-        matched = set()
-        
+        index = set()
         for y in range(m - 1):
             for x in range(n - 1):
                 color = board[y][x]
-                if color == 0:
+                if color == '-':
                     continue
                 
-                if (board[y + 1][x] == color and 
-                    board[y][x + 1] == color and 
-                    board[y + 1][x + 1] == color):
-                    matched.update([(y, x), (y + 1, x), (y, x + 1), (y + 1, x + 1)])
+                if board[y + 1][x] == color and board[y][x + 1] == color and board[y + 1][x + 1] == color:
+                    index.add((x, y))
+                    index.add((x + 1, y))
+                    index.add((x, y + 1))
+                    index.add((x + 1, y + 1))
 
-        if not matched:
+        if not index:
             break
             
-        answer1 += len(matched)
-        for y, x in matched:
-            board[y][x] = 0
-
-        for j in range(n):
-            stack = [board[i][j] for i in range(m) if board[i][j] != 0]
-            for i in range(m - 1,-1,-1):
-                board[i][j] = stack.pop() if stack else 0
+        answer1 += len(index)
+        for x, y in index:
+            board[y][x] = '-'
                     
+        for x in range(n):
+            for y in range(m - 1, -1, -1):
+                if board[y][x] == '-':
+                    ny = y - 1
+                    while ny >= 0 and board[ny][x] == '-':
+                        ny -= 1
+                    
+                    if ny >= 0:
+                        board[y][x] = board[ny][x]
+                        board[ny][x] = '-'
     return answer1
