115103dc03b6bfeff62998f74f358b6b3c0aac06
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-06-25 14:00:10 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,17 @@
 def solution(n, computers):
     answer = 0
-    visited = [False] * n
-
+    visited = [False]  *  n
+    
     def dfs(x):
         visited[x] = True
-        for y in range(n):
-            if computers[x][y] == 1 and not visited[y]:
-                dfs(y)
-
+        for i in range(n):
+            if computers[x][i] == 1 and not visited[i]:
+                dfs(i)
+        
+        
     for i in range(n):
         if not visited[i]:
             dfs(i)
             answer += 1
-
+    
     return answer
