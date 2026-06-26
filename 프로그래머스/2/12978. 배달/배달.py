b31781bd57af23f5b4e8f904a85b0a2281797378
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-06-26 10:38:23 +0900

    [level 2] Title: 배달, Time: 0.98 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -2,27 +2,27 @@ import heapq
 
 def solution(N, road, K):
     graph = [[] for _ in range(N + 1)]
-
-    for a, b, cost in road:
-        graph[a].append((b, cost))
-        graph[b].append((a, cost))
-
-    distance = [float('inf')] * (N + 1)
+    
+    for a, b, time in road:
+        graph[a].append((time, b))
+        graph[b].append((time, a))
+    
+    distance = [1e9] * (N + 1)
+    
     distance[1] = 0
-
+    
     queue = [(0, 1)]
-
+    
     while queue:
-        dist, now = heapq.heappop(queue)
-        
-        if dist > distance[now]:
-            continue
-        
-        for nxt, cost in graph[now]:
-            n_dist = dist + cost
-            
-            if n_dist < distance[nxt]:
-                distance[nxt] = n_dist
-                heapq.heappush(queue, (n_dist, nxt))
+        dis, now = heapq.heappop(queue)
 
-    return sum(1 for d in distance[1:] if d <= K)
+        for n_dis, n in graph[now]:
+            nxt_dis = dis + n_dis
+            if nxt_dis < distance[n]:
+                distance[n] = nxt_dis
+                heapq.heappush(queue, (nxt_dis, n))
+    count = 0
+    for d in distance:
+        if d <= K:
+            count += 1
+    return count
