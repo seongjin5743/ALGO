b432438274ef705e8ac63f335cd0127ce236e145
seongjiiin.hwang <154050436+seongjin5743@users.noreply.github.com>
2026-06-26 11:25:35 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,9 +1,7 @@
 def solution(brown, yellow):
-    result = brown + yellow
+    space = brown + yellow
     
-    answer = []
-    
-    for i in range(3, result + 1):
-        if result % i == 0:
-            if yellow == (i - 2) * (result // i - 2):
-                return sorted([i, result // i], reverse = True)
+    for i in range(3, space + 1):
+        if space % i == 0:
+            if yellow == (i - 2) * (space // i - 2):
+                return sorted([i, space // i], reverse = True)
